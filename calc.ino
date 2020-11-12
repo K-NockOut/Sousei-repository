60911d7eb0359d823d71774d3aacdf986cc60b1c
K-NockOut <73985732+K-NockOut@users.noreply.github.com>
2020-11-12 17:18:12 +0900

    Update calc.ino

--- calc.ino
+++ calc.ino
@@ -19,28 +19,26 @@ void calc(){
       num2=num2+number;
     } 
   }
-  else if(number=10){ 
-    if(phase==0){
-    }
+else if(number=10){
     if(phase==1){
       phase=2;
       calctype=0;
     }
-    if(phase==2){
+    else if(phase==2||3){
       
       if(calctype==0){
         num1=num1+num2;
         num2=0;
       }
-      if(calctype==1){
+      else if(calctype==1){
         num1=num1-num2;
         num2=0;
       }
-      if(calctype==2){
+      else if(calctype==2){
         num1=num1*num2;
         num2=0;
       }
-      if(calctype==3){
+      else if(calctype==3){
         if(num2==0){
         errortype=0;
         disperror();
@@ -52,30 +50,27 @@ void calc(){
        }
       }
     }
-    
   }
-  else if(number==11){ 
-    if(phase=0){
-    }
-    if(phase=1){
+else if(number=11){
+    if(phase==1){
       phase=2;
       calctype=1;
     }
-    if(phase=2){
+    else if(phase==2||3){
       
       if(calctype==0){
         num1=num1+num2;
         num2=0;
       }
-      if(calctype==1){
+      else if(calctype==1){
         num1=num1-num2;
         num2=0;
-       }
-      if(calctype==2){
+      }
+      else if(calctype==2){
         num1=num1*num2;
         num2=0;
       }
-      if(calctype==3){
+      else if(calctype==3){
         if(num2==0){
         errortype=0;
         disperror();
@@ -88,62 +83,26 @@ void calc(){
       }
     }
   }
-  else if(number==12){ 
-    if(phase==0){
-    }
+else if(number=12){
     if(phase==1){
       phase=2;
       calctype=2;
     }
-    if(phase==2){
+    else if(phase==2||3){
       
       if(calctype==0){
         num1=num1+num2;
         num2=0;
       }
-      if(calctype==1){
+      else if(calctype==1){
         num1=num1-num2;
         num2=0;
       }
-      if(calctype==2){
+      else if(calctype==2){
         num1=num1*num2;
         num2=0;
       }
-      if(calctype==3){
-       if(num2==0){
-        errortype=0;
-        disperror();
-        num2=0;
-       }
-       else{
-        num1=num1/num2;
-        num2=0;
-       }
-      }
-    }
-  }
-  else if(number==13){
-    if(phase==0){
-    }
-    if(phase==1){
-      phase=2;
-      calctype=3;
-    }
-    if(phase==2){
-      
-      if(calctype==0){
-        num1=num1+num2;
-        num2=0;
-      }
-      if(calctype==1){
-        num1=num1-num2;
-        num2=0;
-      }
-      if(calctype==2){
-        num1=num1*num2;
-        num2=0;
-      }
-      if(calctype==3){
+      else if(calctype==3){
         if(num2==0){
         errortype=0;
         disperror();
@@ -156,26 +115,55 @@ void calc(){
       }
     }
   }
-  else if(number==14){
-    if(phase==0){
-      
+else if(number=13){
+    if(phase==1){
+      phase=2;
+      calctype=3;
     }
+    else if(phase==2||3){
+      
+      if(calctype==0){
+        num1=num1+num2;
+        num2=0;
+      }
+      else if(calctype==1){
+        num1=num1-num2;
+        num2=0;
+      }
+      else if(calctype==2){
+        num1=num1*num2;
+        num2=0;
+      }
+      else if(calctype==3){
+        if(num2==0){
+        errortype=0;
+        disperror();
+        num2=0;
+       }
+       else{
+        num1=num1/num2;
+        num2=0;
+       }
+      }
+    }
+  }
+else if(number==14){
     if(phase==1){
       if(calctype==0){
         result=num1*2;
-        phase=3;
+        phase=4;
       }
       if(calctype==1){
         result=0;
-        phase=3;
+        phase=4;
       }
       if(calctype==2){
         result=num1*num1;
-        phase=3;
+        phase=4;
       }
       if(calctype==3){
         result=1;
-        phase=3;
+        phase=4;
       }
       else{
         errortype=1;
@@ -185,27 +173,27 @@ void calc(){
     if(phase==2){
       if(calctype==0){
         result=num1+num2;
-        phase=3;
+        phase=4;
       }
       else if(calctype==1){
         result=num1-num2;
-        phase=3;
+        phase=4;
       }
       else if(calctype==2){
         result=num1*num2;
-        phase=3;
+        phase=4;
       }
       else if(calctype==3){
        if(num2==0){
         errortype=0;
         disperror();
         num2=0;
-        phase=1;
+        phase=2;
        }
        else{
         result=num1/num2;
         num2=0;
-        phase=3;
+        phase=4;
        }
       }
       else{
@@ -214,20 +202,19 @@ void calc(){
       }
      }
     }
-  else if(number==15){
+    else if(number==15){ //Cボタンが押されたとき
 
-    if(phase==3){
+    if(phase==4){
       num1=num2=result=0;
       phase=0;
     }
-    else if(phase==2){
+    if(phase==2||3){
       num2=0;
       phase=1;
     }
-    else if(phase==1){
+    if(phase==1){
       num1=0;
       phase=0;
     }
-    else if(phase==0){}
-}
+  }
 }
